6c899266fab0caf64c5c61650e24bc50d8da2181
namnhGru <hoangnamsdc@gmail.com>
2021-12-09 06:54:27 +0700

    add strike function

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
+const todoStyle = {
+    color: 'red',
+    textDecoration: "line-through"
+}
 
 export default function TodoApp() {
-    let [todoList, setTodoList] = useState([])
-    let [newTodo, setNewTodo] = useState('')
+    const [todoList, setTodoList] = useState([])
+    const [newTodo, setNewTodo] = useState('')
 
     function handleSubmit() {
-        let newTodoList = [...todoList, { text: newTodo }]
+        const newTodoList = [...todoList, { text: newTodo, strike: false }]
         setTodoList(newTodoList) 
     }
 
     function handleDelete(index) {
-        let newTodoList = todoList.filter((todo, i) => index !== i)
+        const newTodoList = todoList.filter((todo, i) => index !== i)
         setTodoList(newTodoList)
     }
+
+    function handleStrike(index) {
+        const newTodoList = todoList.reduce((acc, todo, i) => {
+            const { text, strike } = todo
+            if (index === i) {
+                acc.push({text, strike: true})
+                return acc
+            }
+            acc.push(todo)
+            return acc
+        }, [])
+        setTodoList(newTodoList)
+    }
+
     return (
         <div>
             <p>Todo List</p>
@@ -20,20 +38,20 @@ export default function TodoApp() {
                 <input type="text" placeholder="New todo" onChange={e => setNewTodo(e.target.value)}/>
                 <button>Submit</button>
             </form>
-            <TodoList todoList={todoList} handleDelete={handleDelete}></TodoList>
+            <TodoList todoList={todoList} handleDelete={handleDelete} handleStrike={handleStrike}></TodoList>
         </div>
     )
 }
-
 
 function TodoList(props) {
     if (props.todoList.length) {
         return (
             <ul>
                 {props.todoList.map((todo, i) => (
-                    <li key={i}>
-                        {todo.text}
+                    <li key={i} style={todo.strike ? todoStyle : null}>
                         <button onClick={() => props.handleDelete(i)}>X</button>
+                        <button onClick={() => props.handleStrike(i)}>Strike</button>
+                        {todo.text}
                     </li>
 
                 ))}
